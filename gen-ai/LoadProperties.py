177d607e6767d99c7a8856ceb7ff7632c5a4df91
Himanshu Raj <rhimanshu909@gmail.com>
2025-10-06 10:54:02 -0500

    Updated files
    Added description, removed embedding model in config

--- gen-ai/LoadProperties.py
+++ gen-ai/LoadProperties.py
@@ -12,7 +12,7 @@ class LoadProperties:
         self.model_name = js["model_name"]
         self.endpoint = js["endpoint"]
         self.compartment_ocid = js["compartment_ocid"]
-        self.embedding_model_name=js["embedding_model_name"]
+    
 
     def getModelName(self):
             return self.model_name
@@ -23,8 +23,7 @@ class LoadProperties:
     def getCompartment(self):
             return self.compartment_ocid
 
-    def getEmbeddingModelName(self):
-            return self.embedding_model_name
+    
 
 
 
